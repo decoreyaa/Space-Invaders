7ac1108ac8e2a911de8bd1e0e7caa15a5b452787
Decoreya Stewart <decoreya124@gmail.com>
2026-08-08 19:29:56 -0400

    Add formation speed scaling based on remaining enemy count. Also added horizontal bounds and starting position

--- Assets/Scripts/EnemySpawnMovement.cs
+++ Assets/Scripts/EnemySpawnMovement.cs
@@ -6,12 +6,28 @@ public class EnemySpawnMovement : MonoBehaviour
     public float horizontalSpeed = 0.5f;
     public float verticalSpeed = 0.5f;
     private float verticalDirection = 1f;
+    private float startingCount;
+    private float currentCount;
+    private float aliveRatio;
 
+    void Start()
+    {
+        //Gets the number of enemies
+        startingCount = transform.childCount;
+    }
     // Update is called once per frame
     void Update()
     {
-        transform.position += Vector3.left * horizontalSpeed * Time.deltaTime;
+        //Gets current number of enemies
+        currentCount = transform.childCount;
+        //Gets ratio of enemies 
+        aliveRatio = currentCount / startingCount;
+        float multiplier = 2;
 
+        float currentSpeed = horizontalSpeed + (horizontalSpeed * (1f - aliveRatio) * multiplier);
+    
+
+        transform.position += Vector3.left * currentSpeed * Time.deltaTime;
         BoundsCheck();
         Vector2 vertDir = Vector2.up * verticalDirection * verticalSpeed * Time.deltaTime;
         transform.Translate(vertDir);
@@ -38,9 +54,15 @@ public class EnemySpawnMovement : MonoBehaviour
             {
                 verticalDirection = -verticalDirection;
             }
-            if (bottomY < ScreenBounds.Bottom)
-            {
-                verticalDirection = -verticalDirection;
-            }
+        if (bottomY < ScreenBounds.Bottom)
+        {
+            verticalDirection = -verticalDirection;
+        }
+        
+        // //Destroys enemy when it goes outside of screen
+        // if (transform.position.x > 20f || transform.position.x < -30f)
+        // {
+        //     Destroy(gameObject);
+        // }
     }
 }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,28 +8,38 @@ public class PlayerMovement : MonoBehaviour
     public float speed = 5f;
     public float topBound = 4f;
     public float bottomBound = -4f;
+    public float leftBound = -10f;
+    public float rightBound = 3f;
     public Bullet bulletPrefab;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        transform.position = new Vector3(leftBound, 0f, 0f);
     }
 
     // Update is called once per frame
     void Update()
     {
         float v = 0f;
+        float h = 0f;
         if (Input.GetKey(KeyCode.W)) v = 1f;
         if (Input.GetKey(KeyCode.S)) v = -1f;
+        if (Input.GetKey(KeyCode.D)) h = 1f;
+        if (Input.GetKey(KeyCode.A)) h = -1f;
 
-        Vector2 movement = Vector2.up * v * speed * Time.deltaTime;
-        transform.Translate(movement);
+        Vector2 movement = new Vector2(h, v);
+        transform.Translate(movement *speed * Time.deltaTime);
+       
 
         float posY = transform.position.y;
-        float clamp = Mathf.Clamp(transform.position.y, -4f, 4f);
+        float posX = transform.position.x;
+        float clampY = Mathf.Clamp(transform.position.y, bottomBound, topBound);
+        float clampX = Mathf.Clamp(transform.position.x, leftBound, rightBound);
+    
+        //Final Clamp
+        transform.position = new Vector3(clampX, clampY, 0);
 
-        transform.position = new Vector3(transform.position.x, clamp, 0);
-
+        //Registers SPACE to shoot
         if (Input.GetKey(KeyCode.Space) && Time.time >= nextShootTime)
         {
             Shoot();

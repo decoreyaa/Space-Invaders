fa4aeffa2ed0338171fcdae0ca25bab437ce924f
Decoreya Stewart <decoreya124@gmail.com>
2026-08-12 22:13:07 -0400

    Randomize enemy spawn positions using Fisher-Yates shuffle

--- Assets/Scripts/EnemyGridSpawner.cs
+++ Assets/Scripts/EnemyGridSpawner.cs
@@ -1,4 +1,5 @@
-
+using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class EnemyGridSpawner : MonoBehaviour
@@ -13,12 +14,15 @@ public class EnemyGridSpawner : MonoBehaviour
     private bool hasSpawned = false;
     public int enemyCount = 12;
     private int spawnedSoFar = 0;
+    private static readonly System.Random _rng = new System.Random();
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-         
-        
+
+
         float startX = ScreenBounds.Right + spawnOffset;
+        List<Vector2> allPositions = new List<Vector2>();
         // loop through rows and columns, instantiate one enemy per cell
         for (int row = 0; row < rows; row++)
         {
@@ -29,16 +33,17 @@ public class EnemyGridSpawner : MonoBehaviour
                 //subtracted because increasing row should move down the scrteen
                 float y = startPosition.y - (row * verticalSpacing);
 
-                Vector2 spawnPos = new Vector2(x, y);
-                if (spawnedSoFar < enemyCount)
-                {
-                    Instantiate(enemyPrefab, spawnPos, Quaternion.identity, transform);
+                Vector2 spawnPos = new Vector2(x, y);                
+                allPositions.Add(spawnPos);
+                
 
-                }
-                spawnedSoFar++;
 
             }
-           if (spawnedSoFar == enemyCount) break; 
+        }
+        Shuffle(allPositions);
+        for (int i = 0; i < enemyCount; i++)
+        {
+            Instantiate(enemyPrefab, allPositions[i], Quaternion.identity, transform);
         }
         hasSpawned = true;
     }
@@ -57,5 +62,20 @@ public class EnemyGridSpawner : MonoBehaviour
     public bool HasSpawned()
     {
         return hasSpawned;
+    }
+
+    public static void Shuffle<T>(IList<T> list)
+    {
+        int n = list.Count;
+        
+        // Loop backwards through the collection
+        for (int i = n - 1; i > 0; i--)
+        {
+            // Pick a random index from 0 to i (inclusive)
+            int k = _rng.Next(i + 1);
+            
+            // Swap elements using C# tuple deconstruction syntax
+            (list[k], list[i]) = (list[i], list[k]);
+        }
     }
 }
